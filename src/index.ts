a239c42ab21af7e2569ba40b040efc35668b7d6c
Mattias Alm <mattiasalm@gmail.com>
2020-09-07 21:24:15 +0200

    change config order

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,11 @@ import { onLoad, onClick, onPopState, onKeyUp } from './modules/hooks';
 import { setConfig, Config } from './modules/config';
 
 export const init = (newConfig?: Partial<Config>) => {
+  if (!!newConfig) {
+    setConfig(newConfig);
+  }
   window.onload = onLoad;
   window.onclick = onClick;
   window.onpopstate = onPopState;
   window.onkeyup = onKeyUp;
-
-  if (!!newConfig) {
-    setConfig(newConfig);
-  }
 };
